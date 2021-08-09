6eaa9082ecda044f6522cf018962e97f171dd82d
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-08-09 10:48:03 -0700

    Add files via upload

--- Encryption_code_graphic_website.js
+++ Encryption_code_graphic_website.js
@@ -1,16 +1,18 @@
 function setup() {
   createCanvas(2000,8000);
   //background(0,0,0);
-  fill(0,0,0);
-  rect(0,0,2000,3000);
-  rect(0,6000,2000,2000);
-  fill(200,0,0);
-  rect(2000,0,2000,3000);
 }
 
 let mousepos = 25;
 
 function draw() {
+  clear();
+  fill(0,0,0);
+  rect(0,0,2000,1750);
+  rect(0,6000,2000,2000);
+  fill(200,0,0);
+  rect(2000,0,2000,3000);
+  
   frameRate(6000);
   if (mousepos < 25){
     mousepos = 25;
@@ -19,9 +21,22 @@ function draw() {
   
   textSize(150);
   
-  text('Encryption code',200,500);
+  text('Data security: Redefined',200,600);
   
-  text('Encryption code',pos+200,500);
+  textSize(100);
+  fill(mousepos - 100);
+  
+  text('Introducing Encryption code 2.2',200,900);
+  
+  fill(255);
+  
+  text('______________________',(mousepos/2)-1000,1400);
+  text('______________________',(mousepos/-2)+1700,1400);
+  text('______________________',(mousepos/2)-1000,1200);
+  text('______________________',(mousepos/-2)+1700,1200);
+  
+  fill(mousepos - 500);
+  text('All new features',700,1300);
 }
 
 function mouseWheel(event) {
